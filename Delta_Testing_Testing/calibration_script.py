cd8394dc16895d89eaf3e10a067b3b9d9c138ed4
Crocaling <josephpearlman@dpengineering.org>
2020-02-24 10:38:07 -0800

    updated calibration_script

--- Delta_Testing_Testing/calibration_script.py
+++ Delta_Testing_Testing/calibration_script.py
@@ -5,6 +5,28 @@ import time
 od = odrive.find_any()
 if od.serial_number == 59877000491063:
     print("found odrive 1")
+    od.axis0.motor.config.calibration_current = 15.0
+    od.axis0.motor.config.current_lim = 40.0
+    od.axis0.motor.config.requested_current_range = 60.0
+
+    od.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+    time.sleep(32)
+    print("axis error: " + str(od.axis0.error))
+    print("motor error: " + str(od.axis0.motor.error))
+    print("encoder error: " + str(od.axis0.encoder.error))
+    print("controller error: " + str(od.axis0.controller.error))
+
+    od.axis1.motor.config.calibration_current = 15.0
+    od.axis1.motor.config.current_lim = 40.0
+    od.axis1.motor.config.requested_current_range = 60.0
+
+    od.axis1.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+    time.sleep(32)
+    print("axis error: " + str(od.axis1.error))
+    print("motor error: " + str(od.axis1.motor.error))
+    print("encoder error: " + str(od.axis1.encoder.error))
+    print("controller error: " + str(od.axis1.controller.error))
+
 elif od.serial_number == 35623325151307:
     print("found odrive 2")
     od.axis0.motor.config.calibration_current = 15.0
@@ -17,4 +39,4 @@ elif od.serial_number == 35623325151307:
     print("motor error: " + str(od.axis0.motor.error))
     print("encoder error: " + str(od.axis0.encoder.error))
     print("controller error: " + str(od.axis0.controller.error))
-    od.reboot()
+od.reboot()
