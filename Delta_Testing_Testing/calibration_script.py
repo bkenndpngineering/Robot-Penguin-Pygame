ab636af2eecfce1f50b1c845d35b36ba7b1f9b25
no-one <no>
2020-02-24 08:52:09 -0800

    calibration_script

---
+++ Delta_Testing_Testing/calibration_script.py
@@ -0,0 +1,20 @@
+import odrive
+from odrive.enums import *
+import time
+
+od = odrive.find_any()
+if od.serial_number == 59877000491063:
+    print("found odrive 1")
+elif od.serial_number == 35623325151307:
+    print("found odrive 2")
+    od.axis0.motor.config.calibration_current = 15.0
+    od.axis0.motor.config.current_lim = 40.0
+    od.axis0.motor.config.requested_current_range = 60.0
+ 
+    od.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+    time.sleep(32)
+    print("axis error: " + str(od.axis0.error))
+    print("motor error: " + str(od.axis0.motor.error))
+    print("encoder error: " + str(od.axis0.encoder.error))
+    print("controller error: " + str(od.axis0.controller.error))
+    od.reboot()
