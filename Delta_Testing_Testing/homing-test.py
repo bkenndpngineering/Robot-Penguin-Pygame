027958f4716b8ee4e6e6dcbb7a2cfd16c9516599
Crocaling <josephpearlman@dpengineering.org>
2020-03-04 10:54:32 -0800

    added testing

---
+++ Delta_Testing_Testing/homing-test.py
@@ -0,0 +1,12 @@
+from deltaArm import DeltaArm
+import time
+
+arm = DeltaArm()
+arm.initialize()
+print("Successfully initialized! Moving arm now")
+arm.moveToCoordinates(0, 0, -100)
+print("Moving Again")
+arm.moveToCoordinates(100, 100, -100)
+print("shutting down")
+time.sleep(3)
+arm.shutdown()
