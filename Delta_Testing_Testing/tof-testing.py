623f489f6f2482bca5d0398e18ac32019e845ec7
no-one <no>
2020-03-04 09:56:40 -0800

    IT WORKSipython3

--- Delta_Testing_Testing/tof-testing.py
+++ Delta_Testing_Testing/tof-testing.py
@@ -15,13 +15,31 @@ i2c = busio.I2C(board.SCL, board.SDA)
 tca = adafruit_tca9548a.TCA9548A(i2c)
 
 # Create sensor instance.
-sensor = adafruit_vl6180x.VL6180X(tca[6])
+sensor_1 = adafruit_vl6180x.VL6180X(tca[6])
+sensor_2 = adafruit_vl6180x.VL6180X(tca[4])
+
+def get_angle(sensor):
+    # get range from sensor
+    range_mm = sensor.range
+    print('Range: {0}mm'.format(range_mm))
+
+    # horizontal distance from sensor to motor axle in millimeters
+    offset = 60
+    # distance from top of arm to sensor when the arm is horizontal in millimeters
+    horizontal_offset = 45
+
+    A_rad = math.atan((range_mm-horizontal_offset)/offset)
+    A_deg = math.degrees(A_rad)
+    print('Angle: {}deg'.format(A_deg))
+
+    return A_deg
+
 
 # Main loop prints the range and lux every second:
 while True:
     # Read the range in millimeters and print it.
-    range_mm = sensor.range
-    print('Range: {0}mm'.format(range_mm))
+    #range_mm = sensor.range
+    #print('Range: {0}mm'.format(range_mm))
     # Read the light, note this requires specifying a gain value:
     # - adafruit_vl6180x.ALS_GAIN_1 = 1x
     # - adafruit_vl6180x.ALS_GAIN_1_25 = 1.25x
@@ -48,12 +66,17 @@ while True:
     #               range
 
     # horizontal distance from sensor to motor axle in millimeters
-    offset = 60
+    #offset = 60
     # vertical distance measurement taken by sensor
-    range_mm = range_mm
+    #range_mm = range_mm
     # distance from top of arm to sensor when the arm is horizontal in millimeters
-    horizontal_offset = 45
+    #horizontal_offset = 45
 
-    A_rad = math.atan((range_mm-horizontal_offset)/offset)
-    A_deg = math.degrees(A_rad)
-    print('Angle: {}deg'.format(A_deg))
+    #A_rad = math.atan((range_mm-horizontal_offset)/offset)
+    #A_deg = math.degrees(A_rad)
+    #print('Angle: {}deg'.format(A_deg))
+
+    angle_1 = print("sensor 1:", get_angle(sensor_1))
+    angle_2 = print("sensor 2:", get_angle(sensor_2))
+    #angle_3 = get_angle(sensor_3)
+    print()
